c26752380a5c31bbea775ed142fb3b7f248095dd
leopold-wahlbeck <leopoldwahlbeck@gmail.com>
2025-03-03 02:33:25 +0100

    Added score text and test
    Added score text and made it so it adds 1 score every frame for testing

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+    //Text that is TMPro 
+    public TMPro.TextMeshProUGUI scoreText;
+    public int score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += 1;
+        scoreText.text = "Score: " + score;
+        
+    }
+}
