70fe1a5c532a6df07b962e506d37067c5e7f5f6e
adiwir <adrian@avadi.se>
2025-03-10 17:58:31 +0100

    Made Ui for settings

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audio;
    public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
     {
+        Debug.Log("EXIT");
         Application.Quit();
+    }
+
+    public void SetVolume(float vol)
+    {
+        Debug.Log(vol);
+        audio.SetFloat("volume", vol);
     }
 }
