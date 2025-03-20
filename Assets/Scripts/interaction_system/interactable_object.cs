1a430f8dc7381ded3b6a809d7ba5cc5d395d45a8
Alexander Lisborg <alexanderlisborg@gmail.com>
2025-03-20 15:13:07 +0100

    Add interaction system, demo scene and demo prefabs.

---
+++ Assets/Scripts/interaction_system/interactable_object.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// An interactable object. Attach directly for UnityEvent functionality or
+/// extend to specify behaviour for a specific subclass of interactable objects.
+/// </summary>
+public class interactable_object : MonoBehaviour
+{
+    /// <summary>
+    /// Text that describes the interaction.
+    /// </summary>
+    [SerializeField] protected string _interactionText = "Press E to interact";
+    /// <summary>
+    /// Triggered by interactor when it starts an interaction with this interactable.
+    /// </summary>
+    [SerializeField] private UnityEvent<interactor> _onInteractStart;
+    /// <summary>
+    /// Triggered by interactor when it ends an interaction with this interactable.
+    /// </summary>
+    [SerializeField] private UnityEvent<interactor> _onInteractEnd;
+    /// <summary>
+    /// IsTrigger interactable objects does not affect the active interactions stack
+    /// nor the active interactors list and only triggers begin interaction and end
+    /// interaction events whenever an interaction is begun.
+    /// </summary>
+    [SerializeField] private bool _isTrigger;
+    /// <summary>
+    /// Specifies how many interacors to allow simultaneously. When that number of
+    /// interactors are actively interacting with this interactable, stop all other
+    /// incoming interaction attempts.
+    /// </summary>
+    [SerializeField] private int _maxNumberOfInteractors = 1;
+    /// <summary>
+    /// Active interactors.
+    /// </summary>
+    private List<interactor> _activeInteractors = new List<interactor>();
+    /// <summary>
+    /// Invoke onBeginInteraction if not full. If the interactable is at max active interactors, the 
+    /// interaction is canceled via the interactor. If istrigger, does not update the active 
+    /// interactors stack.
+    /// </summary>
+    /// <param name="interactor">The interactor attempting interaction with this interactable.</param>
+    public void BeginInteraction(interactor interactor)
+    {
+        if(_activeInteractors.Count < _maxNumberOfInteractors && _activeInteractors.Contains(interactor) == false)
+        {
+            if(!_isTrigger){_activeInteractors.Add(interactor);}
+            _onInteractStart?.Invoke(interactor);
+        } else{interactor.CancelInteraction(this);}
+    }
+    /// <summary>
+    /// Invoke onEndInteraction
+    /// </summary>
+    /// <param name="interactor"></param>
+    public void EndInteraction(interactor interactor)
+    {
+        if(!_isTrigger){RemoveActiveInteractor(interactor);}
+        _onInteractEnd?.Invoke(interactor);
+    }
+    /// <summary>
+    /// Getter.
+    /// </summary>
+    /// <returns>Text that describes the interaction.</returns>
+    public string GetInteractionText(){return _interactionText;}
+    /// <summary>
+    /// Getter.
+    /// </summary>
+    /// <returns>Wether the interactable object has enabled is trigger or not.</returns>
+    public bool IsTrigger(){return _isTrigger;}
+    /// <summary>
+    /// Removes active interactor from list. Use interactor.TryEndInteraction instead to end interactions.
+    /// </summary>
+    /// <param name="interactor">The interactor to remove from active interactors list.</param>
+    public void RemoveActiveInteractor(interactor interactor){_activeInteractors.Remove(interactor);}
+    /// <summary>
+    /// Add active interactor. Use interactor.BeginInteraction instead for beginning interactions.
+    /// </summary>
+    /// <param name="interactor"></param>
+    public void AddActiveInteractor(interactor interactor){_activeInteractors.Add(interactor);}
+}
---
+++ Assets/Scripts/interaction_system/wagon_storage_interactable.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class wagon_storage_interactable : interactable_object
+{
+    [SerializeField]private string _dropOffText = "Press E to drop off";
+    [SerializeField]private string _wagonFullText = "Wagon is full!";
+    [SerializeField]private string _wagonEmptyText = "Wagon is empty!";
+    [SerializeField]private wagon_storage _wagon;
+    public string DropOffText;
+    private string _ogInteractionText;
+    void Start()
+    {
+        _ogInteractionText = _interactionText;
+        DropOffText = _dropOffText;
+    }
+
+    public void HandleInteractTrigger(interactor context)
+    {
+        _interactionText = _ogInteractionText;
+        bool success = context.TryPop(out var activeInteractable);
+        if(success)// Attempt drop off
+        {
+            
+            if(!_wagon.IsFull())
+            {
+                DropOffText = _dropOffText;
+                activeInteractable.RemoveActiveInteractor(context);
+                _wagon.AddPackage(activeInteractable.gameObject);
+            } 
+            else
+            {
+                DropOffText = _wagonFullText;
+                context.Push(activeInteractable);
+            }
+            activeInteractable.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");            
+        } 
+        else// Attempt pickup. Does nothing if wagon is empty 
+        {
+            GameObject package = _wagon.RemovePackage();
+            if (package!=null)
+            {
+                DropOffText = _dropOffText;
+                package.layer = LayerMask.NameToLayer("Ignore Raycast");
+                if(package.TryGetComponent<interactable_object>(out var interactable))
+                {
+                    context.BeginInteraction(interactable);
+                } else {Debug.Log("Warning : Tried to pick up a package that is not interactable. Attach an " +
+                typeof(interactable_object).ToString()+" component to the package gameobject '" + package.name +"'.");}
+            } else {_interactionText=_wagonEmptyText;}
+        }
+    }   
+}
