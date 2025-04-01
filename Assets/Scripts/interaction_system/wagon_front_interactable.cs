7dbd3e865e60e96bdcebea9570ef48ddf6d993b1
Alexander Lisborg <alexanderlisborg@gmail.com>
2025-04-01 14:26:24 +0200

    bug fixes in interactable. Animations are now updated correctly and if you're holding a package and try to steer the wagon, it will drop.

--- Assets/Scripts/interaction_system/wagon_front_interactable.cs
+++ Assets/Scripts/interaction_system/wagon_front_interactable.cs
@@ -4,13 +4,39 @@ public class wagon_front_interactable : interactable_object
 {
     [SerializeField] private wagon_controller _wagon;
     [SerializeField] private Transform _interactionTextPosition;
+    [SerializeField] private string _holdingWagonString = "holdingWagon";
     public Transform GetInteractionTextPosition(){if(_interactionTextPosition!=null)return _interactionTextPosition; else return transform;}
     public void OnInteractStart(interactor context)
     {
+        // Disable player collider
+        if (context.TryGetComponent<Collider>(out var collider))
+        {
+            collider.enabled = false;
+        }
+        
+        if(context.ActiveInteractionsCount() > 0)
+        {
+            context.TryPop(out var temp);
+            context.TryEndInteraction();
+            context.Push(temp);
+        }
+        // Update holdingWagon
+        observable_value_collection obvc = context.GetPlayerObservableValueCollection();
+        if(obvc!=null){obvc.InvokeBool(_holdingWagonString, true);}
+        // Interact with wagon
         _wagon.Interact(context.gameObject);
     }
     public void OnInteractEnd(interactor context)
     {
+        // Enable player collider
+        if (context.TryGetComponent<Collider>(out var collider))
+        {
+            collider.enabled = true;
+        }
+        // Update holdingWagon
+        observable_value_collection obvc = context.GetPlayerObservableValueCollection();
+        if(obvc!=null){obvc.InvokeBool(_holdingWagonString, false);}
+        // Interact with wagon
         _wagon.Interact(context.gameObject);
     }
 }
