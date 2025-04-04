0417fa43a300cc35993aa620a504d6e1e732bfa5
XanJan <annamajberger@gmail.com>
2025-04-04 20:12:14 +0200

    Continued

--- Assets/level_create.cs
+++ Assets/level_create.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class level_create : MonoBehaviour
@@ -5,6 +6,7 @@ public class level_create : MonoBehaviour
     [SerializeField]
     private GameObject[] neighborhoods;
     private int neighborhoodOrder = 1;
+    private List<GameObject> instantiateNeighborhoods;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +16,19 @@ public class level_create : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AddNeighborhoodToLevel();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
     }
     private void AddNeighborhoodToLevel()
     {
-        Instantiate(neighborhoods[neighborhoodOrder % neighborhoods.Length], new Vector3(0, 0, 38f * neighborhoodOrder), Quaternion.identity);
+        GameObject neigh = Instantiate(neighborhoods[neighborhoodOrder % neighborhoods.Length], new Vector3(0, 0, 38f * neighborhoodOrder), Quaternion.identity);
+        instantiateNeighborhoods.Add(neigh);
         neighborhoodOrder++;
+
     }
 
     private void RemoveNeighborhoodFromLevel(GameObject stage)
