358db6a644595a5adf3b83288d4d22d0c1bdc369
Anna Majberger <annamajberger@gmail.com>
2025-04-09 15:44:41 +0200

    Started on sound effects

---
+++ Assets/sound_manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class sound_manager : MonoBehaviour
+{
+    [Header("___Audio Sources___")]
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    [Header("___Audio Clips___")]
+    public AudioClip[] sfxClips;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
