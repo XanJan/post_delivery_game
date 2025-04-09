1f54c9e6f3f79c3a5ec6bc14b9143372a8b6a879
Anna Majberger <annamajberger@gmail.com>
2025-04-09 10:30:28 +0200

    Fixed score in end scene

--- Assets/Scripts/end_menu.cs
+++ Assets/Scripts/end_menu.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class end_screen : MonoBehaviour
 {
+    public TMPro.TextMeshProUGUI scoreText;
     public void returnToStart()
     {
         SceneManager.LoadSceneAsync(0);
+    }
+
+    private void Start()
+    {
+        updateScore();
+    }
+
+    public void updateScore()
+    {
+        scoreText.text = "Score: " + score_manager.Instance.totalPackageScore;
     }
 }
