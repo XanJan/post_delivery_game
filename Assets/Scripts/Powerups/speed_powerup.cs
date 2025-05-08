270ffad63e282b045c3131a7cd1202f634bc0e51
XanJan <annamajberger@gmail.com>
2025-05-08 23:41:01 +0200

    Fixed speed power up with time interval

--- Assets/Scripts/Powerups/speed_powerup.cs
+++ Assets/Scripts/Powerups/speed_powerup.cs
@@ -1,11 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "NewPowerup", menuName = "Powerup/Speed Powerup")]
 public class speed_powerup : powerup
 {
     public float speedMultiplier;
+    public float time;
     public override void ApplyPowerup(GameObject target)
     {
-        target.GetComponent<player_movement>().OnUpdateMoveSpeedMultiplierPickup("", speedMultiplier);
-        
+        player_movement script = target.GetComponent<player_movement>();
+        script.StartCoroutine(TemporarySpeedBoost(script));
+
+    }
+    private IEnumerator TemporarySpeedBoost(player_movement target)
+    {
+        target.OnUpdateMoveSpeedBase("", speedMultiplier);
+        yield return new WaitForSeconds(time);
+        target.OnUpdateMoveSpeedBase("", 5f); // Reset to normal speed
     }
 }
