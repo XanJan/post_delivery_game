2b33a0e363e04ccf17aacb36fbbd5078aabd8118
Anna Majberger <annamajberger@gmail.com>
2025-05-08 11:47:39 +0200

    Some changes

--- Assets/Scripts/Powerups/speed_powerup.cs
+++ Assets/Scripts/Powerups/speed_powerup.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "NewPowerup", menuName = "Powerup/PowerupItem")]
 public class speed_powerup : powerup
 {
-    public override void ApplyPower()
+    public override void ApplyPowerup(GameObject target)
     {
         // Apply the speed boost to the player
         //player.ApplySpeedBoost(5, duration);
