ffa072fa70ec6c3c69bcb20d1be9e8c481792cbe
leopold-wahlbeck <leopoldwahlbeck@gmail.com>
2025-05-13 01:37:51 +0200

    Modified SoundManager
    Made changes to SoundManager to match the needs of our project.

--- Assets/Scripts/sound_scripts/SoundManager.cs
+++ Assets/Scripts/sound_scripts/SoundManager.cs
@@ -1,36 +1,43 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+public enum SoundType
+{
+    ButtonClick,
+    GateOpenSuccess,
+    GateOpenFail,
+    PackageDelivered,
+    DeliveryZoneOneCompleted,
+    DeliveryZoneTwoCompleted,
+    DeliveryZoneThreeCompleted,
+    DeliveryZoneFourCompleted,
+    NeighborhoodCompleted,
+    PackageThrown,
+
+}
+
+[RequireComponent(typeof(AudioSource))]
+
 public class SoundManager : MonoBehaviour
 {
-    private static SoundManager _instance;
-    public static SoundManager Instance
+    [SerializeField] private List<AudioClip> soundClips;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
     {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<SoundManager>();
-                if (_instance == null)
-                {
-                    GameObject soundManagerObject = new GameObject("SoundManager");
-                    _instance = soundManagerObject.AddComponent<SoundManager>();
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
-            }
-            return _instance;
-        }
-    }
-    public class SoundEffect
-    {
-        public string name;
-        public AudioClip clip;
-        public float volume = 1.0f;
-        public float pitch = 1.0f;
-        public bool loop = false;
-        [HideInInspector] public AudioSource source;
+        instance = this;
     }
 
-    public SoundEffect[] soundEffects;
-    
+    public void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1.0f)
+    {
+        instance.audioSource.PlayOneShot(instance.soundClips[(int)sound], volume);
+    }
+
+
 }
