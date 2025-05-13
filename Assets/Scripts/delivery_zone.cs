52d3a6782d0a0eda2c469e8b10b3cdf73ff4001f
XanJan <annamajberger@gmail.com>
2025-05-13 08:47:42 +0200

    Some fixes

--- Assets/Scripts/delivery_zone.cs
+++ Assets/Scripts/delivery_zone.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
@@ -250,7 +250,7 @@ public class delivery_zone : MonoBehaviour
                 /*if (go.TryGetComponent<interactable_object>(out var interactable))
                 {
                     
-                }*/
+                }*//*
                 
                 if (go.TryGetComponent<Rigidbody>(out var rb))
                 {
@@ -307,4 +307,324 @@ public class delivery_zone : MonoBehaviour
     {
         return isCompleted;
     }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.Mathematics;
+using System.Linq;
+
+public class delivery_zone : MonoBehaviour
+{
+    private HashSet<GameObject> detectedPackages = new HashSet<GameObject>();
+    private Renderer areaRenderer;
+    int yOffset = 0;
+    float lastYPos;
+    public Color red = Color.red;
+    public Color green = Color.green;
+    public int maxPackages = 5;
+    public TextMeshPro textDisplay;
+    private enum packageType
+    {
+        small,
+        big
+    }
+
+    /// <summary>
+    /// The points where the dropped off packages should appear. 
+    /// </summary>
+    [SerializeField] private List<Transform> _dropOffPoints;
+    private neighborhood_manager _neighborhoodManager;
+    private bool isCompleted = false;
+
+    void Start()
+    {
+        areaRenderer = GetComponent<Renderer>();
+        areaRenderer.material.color = red;
+        UpdateText();
+
+        // Find the associated neighborhood manager
+        FindCorrectNeighborhoodManager();
+    }
+
+    private void FindCorrectNeighborhoodManager()
+    {
+        // find namespace containing name "DELIVERY ZONES"
+        Transform deliveryZonesParent = null;
+        Transform current = transform.parent;
+
+        // find "DELIVERY ZONES"
+        while (current != null)
+        {
+            if (current.name.Contains("DELIVERY ZONES"))
+            {
+                deliveryZonesParent = current;
+                break;
+            }
+            current = current.parent;
+        }
+
+        // If found DELIVERY ZONES parent, go up one more level to find the neighborhood
+        if (deliveryZonesParent != null && deliveryZonesParent.parent != null)
+        {
+            // Try find neighborhood manager in the parent of DELIVERY ZONES
+            _neighborhoodManager = deliveryZonesParent.parent.GetComponent<neighborhood_manager>();
+
+            // If not found there, search in the grandparent
+            if (_neighborhoodManager == null && deliveryZonesParent.parent.parent != null)
+            {
+                _neighborhoodManager = deliveryZonesParent.parent.parent.GetComponent<neighborhood_manager>();
+            }
+        }
+
+        // If still not found, perform a more expensive search in the entire hierarchy upward
+        if (_neighborhoodManager == null)
+        {
+            current = transform;
+            while (current != null)
+            {
+                _neighborhoodManager = current.GetComponent<neighborhood_manager>();
+                if (_neighborhoodManager != null)
+                    break;
+                current = current.parent;
+            }
+        }
+
+        // Final fallback - find neighborhood by closest proximity
+        if (_neighborhoodManager == null)
+        {
+            Debug.LogWarning($"Delivery zone {gameObject.name} couldn't find a neighborhood manager in its hierarchy. Trying to find by proximity...");
+
+            // Get all neighborhood managers
+            neighborhood_manager[] managers = FindObjectsOfType<neighborhood_manager>();
+            if (managers.Length > 0)
+            {
+                float closestDistance = float.MaxValue;
+                foreach (neighborhood_manager manager in managers)
+                {
+                    float distance = Vector3.Distance(transform.position, manager.transform.position);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        _neighborhoodManager = manager;
+                    }
+                }
+
+                if (_neighborhoodManager != null)
+                {
+                    Debug.LogWarning($"Assigned delivery zone {gameObject.name} to neighborhood {_neighborhoodManager.GetNeighborhoodId()} by proximity (distance: {closestDistance})");
+                }
+            }
+        }
+
+        if (_neighborhoodManager != null)
+        {
+            Debug.Log($"Delivery zone {gameObject.name} is associated with neighborhood {_neighborhoodManager.GetNeighborhoodId()}");
+
+            _neighborhoodManager.RegisterDeliveryZone(this);
+        }
+        else
+        {
+            Debug.LogError($"Delivery zone {gameObject.name} could not be associated with any neighborhood manager!");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<pickup_interactable>(out var res))
+        {
+            if (res.GetInteractors().Length == 0) // Noone is interacting with the package
+            {
+                packageType package = packageType.small;
+                TryConsumePackage(other.gameObject, package);
+            }
+        }
+
+        else if (other.TryGetComponent<big_package_interactable>(out var beegRes))
+        {
+            if (beegRes.GetInteractors().Length == 0)
+            {
+                packageType package = packageType.big;
+                TryConsumePackage(other.gameObject, package);
+            }
+        }
+    }
+    /// <summary>
+    /// If a player stands on the zone and the zone is not full, try to take a package 
+    /// from its active interactions.
+    /// </summary>
+    void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent<player_interactor>(out var player))
+        {
+            if (player.TryPeekInteraction(out var res))
+            {
+                switch (res)
+                {
+                    case pickup_interactable:
+                        if (detectedPackages.Count < maxPackages && player.TryForceInteractEnd(null, out var top))
+                        {
+                            packageType package = packageType.small;
+                            TryConsumePackage(top.gameObject, package);
+                        }
+                        break;
+                }
+            }
+        }
+
+        else if (other.TryGetComponent<player_interactor>(out var beegPlayer))
+        {
+            if (player.TryPeekInteraction(out var beegRes))
+            {
+                switch (beegRes)
+                {
+                    case pickup_interactable:
+                        if (detectedPackages.Count < maxPackages && beegPlayer.TryForceInteractEnd(null, out var top))
+                        {
+                            packageType package = packageType.big;
+                            TryConsumePackage(top.gameObject, package);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Helper method, tries to consumes a package. If zone is not full, 
+    /// update score and text and place package on zone. If the zone becomes 
+    /// full, change color and notify current game event.
+    /// </summary>
+    /// <param name="go">GameObject to "consume" and Destroy.</param>
+    private void TryConsumePackage(GameObject go, packageType pType)
+    {
+        if (detectedPackages.Count < maxPackages && !detectedPackages.Contains(go))
+        {
+            lastYPos = (float)(0.5 * (detectedPackages.Count + yOffset));
+            detectedPackages.Add(go);
+            switch (pType)
+            {
+                case packageType.small:
+                    score_manager.Instance.AddScore(1);
+                    break;
+                case packageType.big:
+                    score_manager.Instance.AddScore(5);
+                    break;
+
+            }
+
+            UpdateText();
+            if (detectedPackages.Count >= maxPackages && !isCompleted)
+            {
+                isCompleted = true;
+                areaRenderer.material.color = green;
+
+                // IMPORTANT CHANGE: Directly notify only this zone's neighborhood manager
+                if (_neighborhoodManager != null)
+                {
+                    _neighborhoodManager.ZoneCompleted(this);
+                    Debug.Log($"Zone completed in neighborhood {_neighborhoodManager.GetNeighborhoodId()}");
+                }
+                else
+                {
+                    try { game_events.current.PackageComplete(); }
+                    catch (NullReferenceException) { Debug.Log("Warning, game_event instance is null. Game events may not register properly."); }
+                }
+            }
+
+            if (go != null)
+            {
+                go.transform.SetParent(transform);
+
+                //go.transform.eulerAngles =new Vector3(0,new int[]{0,90,180,270}[detectedPackages.Count % 4],0);
+                go.transform.localPosition = (_dropOffPoints != null && _dropOffPoints.Count > 0) ?
+                    _dropOffPoints[(detectedPackages.Count - 1) % _dropOffPoints.Count].localPosition :
+                        new Vector3(new float[] { 0.1f, 0f, -0.1f, 0f }[detectedPackages.Count % 4], lastYPos, new float[] { 0f, 0.1f, 0f, -0.1f }[detectedPackages.Count % 4]);
+
+                if (go.TryGetComponent<interactable_object>(out var interactable))
+                    go.transform.eulerAngles = new Vector3(0, new int[] { 0, 90, 180, 270 }[detectedPackages.Count % 4], 0);
+                go.transform.localScale = new Vector3(4, 1, 4);
+                interactable.AllowInteractions = false;
+                switch (pType)
+                {
+                    case packageType.small:
+                        go.transform.localPosition = (_dropOffPoints != null && _dropOffPoints.Count > 0) ?
+                        _dropOffPoints[detectedPackages.Count - 1 % _dropOffPoints.Count].localPosition :
+                        new Vector3(new float[] { 0.1f, 0f, -0.1f, 0f }[detectedPackages.Count % 4], lastYPos, new float[] { 0f, 0.1f, 0f, -0.1f }[detectedPackages.Count % 4]);
+                        break;
+                    case packageType.big:
+                        UpdateYPos();
+                        go.transform.localPosition = new Vector3(+0, +lastYPos, +0);
+                        yOffset += 1;
+                        break;
+                }
+
+
+
+                /*if (go.TryGetComponent<interactable_object>(out var interactable))
+                {
+                    
+                }*/
+
+                if (go.TryGetComponent<Rigidbody>(out var rb))
+                {
+                    rb.isKinematic = true;
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Helper method, updates text to match detected package count.
+    /// </summary>
+    private void UpdateText()
+    {
+        if (textDisplay != null)
+        {
+            textDisplay.text = $"{detectedPackages.Count}/{maxPackages}";
+        }
+    }
+
+    private void UpdateYPos()
+    {
+        yOffset += 0;
+        lastYPos = (float)(0.5 * (detectedPackages.Count + yOffset));
+    }
+
+    //way to sum total score in a gamehandler (add way of counting scores for different types of packages)
+    public int FinalZoneScore()
+    {
+        if (detectedPackages.Count >= maxPackages)
+        {
+            return detectedPackages.Count;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    // Get the neighborhood ID this delivery zone belongs to
+    public int GetNeighborhoodId()
+    {
+        if (_neighborhoodManager != null)
+        {
+            return _neighborhoodManager.GetNeighborhoodId();
+        }
+        return -1; // Not associated with any neighborhood
+    }
+
+    // Check if this delivery zone is completed
+    public bool IsCompleted()
+    {
+        return isCompleted;
+    }
+
+    public int GetCurrentPackages()
+    {
+        return detectedPackages.Count;
+    }
 }
+
+
--- Assets/Scripts/neighborhood_manager.cs
+++ Assets/Scripts/neighborhood_manager.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using UnityEngine;
 
 public class neighborhood_manager : MonoBehaviour
@@ -10,21 +10,21 @@ public class neighborhood_manager : MonoBehaviour
     private int completedZones = 0;
     private bool isCompleted = false;
     private List<delivery_zone> deliveryZones = new List<delivery_zone>();
-    private int deliveryZonesCount;
-    private int finishedDeliveryZones;
+    //private int deliveryZonesCount;
+    //private int finishedDeliveryZones;
     void Start()
     {
-        deliveryZonesCount = this.transform.childCount;
+        //deliveryZonesCount = this.transform.childCount;
         Debug.Log(deliveryZones);
 
         game_events.current.onPackageTrigger += CountFinishedDeliveryZones;
-        game_events.current.RaiseNeighborhoodGenerated();
+        //game_events.current.RaiseNeighborhoodGenerated();
     }
 
     private void CountFinishedDeliveryZones()
     {
-        finishedDeliveryZones++;
-        if (deliveryZonesCount <= finishedDeliveryZones)
+        //finishedDeliveryZones++;
+        //if (deliveryZonesCount <= finishedDeliveryZones)
         // Register neighborhood with the tracker
         neighborhood_delivery_tracker.Instance.RegisterNeighborhood(neighborhoodId);
         
@@ -103,6 +103,108 @@ public class neighborhood_manager : MonoBehaviour
             // Notify that neighborhood is finished
             game_events.current.NeighborhoodFinished();
             
+            Debug.Log($"Neighborhood {neighborhoodId} is now complete!");
+        }
+    }
+    public int GetNeighborhoodId()
+    {
+        return neighborhoodId;
+    }
+}*/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class neighborhood_manager : MonoBehaviour
+{
+    [SerializeField] private int neighborhoodId; // identifier for this neighborhood
+    [SerializeField] private Transform deliveryZonesParent; // Parent transform containing all delivery zones
+
+    private int totalZones;
+    private int completedZones = 0;
+    private bool isCompleted = false;
+    private List<delivery_zone> deliveryZones = new List<delivery_zone>();
+
+    void Start()
+    {
+        // Register neighborhood with the tracker
+        neighborhood_delivery_tracker.Instance.RegisterNeighborhood(neighborhoodId);
+
+        // Find all delivery zones in neighborhood
+        if (deliveryZonesParent == null)
+        {
+            // Try to find the delivery zones parent by name
+            Transform[] transforms = GetComponentsInChildren<Transform>();
+            foreach (Transform t in transforms)
+            {
+                if (t.name.Contains("DELIVERY ZONES"))
+                {
+                    deliveryZonesParent = t;
+                    break;
+                }
+            }
+        }
+
+        if (deliveryZonesParent != null)
+        {
+            // Find and store all delivery zones in this neighborhood
+            foreach (Transform child in deliveryZonesParent)
+            {
+                delivery_zone zone = child.GetComponent<delivery_zone>();
+                if (zone != null)
+                {
+                    totalZones++;
+                }
+            }
+
+            Debug.Log($"Neighborhood {neighborhoodId} has {totalZones} delivery zones");
+        }
+        else
+        {
+            Debug.LogWarning($"No delivery zones parent found in neighborhood {neighborhoodId}");
+        }
+    }
+
+    // This method allows delivery zones to register themselves with this manager
+    public void RegisterDeliveryZone(delivery_zone zone)
+    {
+        if (!deliveryZones.Contains(zone))
+        {
+            deliveryZones.Add(zone);
+
+            // Only increment the counter if we didn't find these zones 
+            if (deliveryZonesParent == null || !zone.transform.IsChildOf(deliveryZonesParent))
+            {
+                totalZones++;
+                Debug.Log($"Neighborhood {neighborhoodId}: Registered zone {zone.name}, now tracking {totalZones} zones");
+            }
+        }
+    }
+
+    // called when a delivery zone is completed
+    public void ZoneCompleted(delivery_zone zone)
+    {
+        // Verify zone belongs to this neighborhood
+        if (!deliveryZones.Contains(zone))
+        {
+            Debug.LogWarning($"Zone {zone.name} reported completion to neighborhood {neighborhoodId} but isn't part of this neighborhood");
+            return;
+        }
+
+        completedZones++;
+        Debug.Log($"Neighborhood {neighborhoodId}: {completedZones}/{totalZones} zones completed");
+
+        // Check if all delivery zones in neighborhood are completed
+        if (totalZones > 0 && completedZones >= totalZones && !isCompleted)
+        {
+            isCompleted = true;
+
+            // Mark neighborhood as completed in tracker
+            neighborhood_delivery_tracker.Instance.SetNeighborhoodComplete(neighborhoodId);
+
+            // Notify that neighborhood is finished
+            game_events.current.NeighborhoodFinished();
+
             Debug.Log($"Neighborhood {neighborhoodId} is now complete!");
         }
     }
